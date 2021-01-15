5a369c72fd059c5baef90cc0005280ea95463554
Michael Casey <caseymichaelt1893@gmail.com>
2021-01-15 17:45:01 -0500

    updates

--- translator/src/App.js
+++ translator/src/App.js
@@ -5,8 +5,35 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import Validate from './validate.js';
+
+// Imports the Google Cloud client library
+const {Translate} = require('@google-cloud/translate').v2;
+
+// Creates a client
+const translate = new Translate();
+
+/**
+ * TODO(developer): Uncomment the following lines before running the sample.
+ */
+ const text = 'The text to translate, e.g. Hello, world!';
+ const target = 'The target language, e.g. ru';
+
+async function translateText() {
+  // Translates the text into the target language. "text" can be a string for
+  // translating a single piece of text, or an array of strings for translating
+  // multiple texts.
+  let [translations] = await translate.translate(text, target);
+  translations = Array.isArray(translations) ? translations : [translations];
+  console.log('Translations:');
+  translations.forEach((translation, i) => {
+    console.log(`${text[i]} => (${target}) ${translation}`);
+  });
+}
+
+translateText();
 class App extends React.Component{
   constructor(props){
+    
     super(props);
     this.state={
       text: {
