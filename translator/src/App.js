c64ca7fb64312417b4bafa22b995189f4049e4db
Keith Godfrey <keithgodfrey@Keiths-MacBook-Pro.local>
2021-01-19 15:53:12 -0500

    tranlate updates

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,101 +1,101 @@
-import './App.css';
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Validate from './validate.js';
-import CREDENTIALS from './credentials.json';
-const {Translate} = require('@google-cloud/translate').v2;
+import "./App.css";
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Validate from "./validate.js";
+import CREDENTIALS from "./credentials.json";
+const { Translate } = require("@google-cloud/translate").v2;
 
-
-//export GOOGLE_APPLICATION_CREDENTIALS="/Users/michaelcasey/mec-assignment-10/translator/secrets.json"
-
-async function translateText(translate, text, target) {
+async function translateText(translate, text, target, state) {
   // Translates the text into the target language. "text" can be a string for
   // translating a single piece of text, or an array of strings for translating
   // multiple texts.
   let [translations] = await translate.translate(text, target);
   translations = Array.isArray(translations) ? translations : [translations];
-  console.log('Translations:');
-  translations.forEach((translation, i) => {
-    console.log(`${text[i]} => (${target}) ${translation}`);
-  });
+  state.text.output = translations[0];
 }
 
-class App extends React.Component{
-  constructor(props){
-    
-
-    
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
+    this.state = {
       text: {
-        value: '',
-        error: ''
-      }
-    }
+        value: "",
+        error: "",
+        output: "",
+      },
+    };
   }
-  submitForm(event){
-    
-
-// Imports the Google Cloud client library
-
-
-// Creates a client
-const translate = new Translate({credentials:CREDENTIALS});
-
-/**
- * TODO(developer): Uncomment the following lines before running the sample.
- */
- const text = 'Hello, world!';
- const target = 'es';
-
-
-translateText(translate, text, target);
-    event.preventDefault();
+  changeState() {
+    const translate = new Translate({ credentials: CREDENTIALS });
+    const text = this.state.text.value;
+    const target = "es";
     let state = this.state;
     state.text.error = Validate(this.state.text.value);
-    this.setState(state);
-  }
-  updateForm(event){
-    this.setState({
-      text:{
-        value: event.target.value,
-        error:this.state.text.error
-      }
-    });
-
-  }
-  render(){
-    return (
-    <div className="App">
-      <Grid container>
-        <Grid item xs={12} align="center">
-          <Typography variant="h2">Translator</Typography>
-        </Grid>
-        <Grid item xs={12}>
-      <form onSubmit={(event) =>{this.submitForm(event);}}>
-      <TextField 
-      fullWidth
-       placeholder="Enter text here" 
-       variant="outlined" 
-       value={this.state.text.value}
-       helperText={this.state.text.error}
-       error={this.state.text.error !==""} 
-       onChange={(event) => {this.updateForm(event)}}/>
-      <Button 
-      type="submit" 
-      variant="contained">
-        Translate
-        </Button>
-      </form>
-      </Grid>
-      </Grid>
-    </div>
-  );
+    translateText(translate, text, target, state);
+    console.log(state)
+    // this.setState(state);
   }
   
+  submitForm(event) {
+    event.preventDefault();
+    let state = this.state;
+    this.setState(state);
+  }
+  updateForm(event) {
+    this.setState({
+      text: {
+        value: event.target.value,
+        error: this.state.text.error,
+      },
+    });
+  }
+  componentWillUpdate(){   
+     this.changeState();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Grid container>
+          <Grid item xs={12} align="center">
+            <Typography variant="h2">Translator</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <form
+              onSubmit={(event) => {
+                this.submitForm(event);
+              }}
+            >
+              <TextField
+                fullWidth
+                placeholder="Enter text here"
+                variant="outlined"
+                value={this.state.text.value}
+                helperText={this.state.text.error}
+                error={this.state.text.error !== ""}
+                onChange={(event) => {
+                  this.updateForm(event);
+                }}
+              />
+              <Button type="submit" variant="contained">
+                Translate
+              </Button>
+            </form>
+            <p>{this.state.text.output}</p>
+            {/* <TextField 
+      fullWidth
+       variant="outlined" 
+       value={this.state.text.output}
+       helperText={this.state.text.error}
+       error={this.state.text.error !==""} 
+       /> */}
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
 }
 
 export default App;
